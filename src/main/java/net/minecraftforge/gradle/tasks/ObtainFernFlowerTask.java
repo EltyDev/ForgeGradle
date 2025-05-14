97e6cb6877739d6421a794d792a0bd5bb5962916
Élise Valentin <venodez@gmail.com>
2025-05-14 09:49:19 +0200

    Print hunk lines

--- src/main/java/net/minecraftforge/gradle/tasks/ObtainFernFlowerTask.java
+++ src/main/java/net/minecraftforge/gradle/tasks/ObtainFernFlowerTask.java
@@ -44,9 +44,6 @@ public class ObtainFernFlowerTask extends CachedTask
 
         while ((entry = zin.getNextEntry()) != null)
         {
-            System.out.println(entry.getName());
-            System.out.println(StringUtils.lower(entry.getName()).endsWith("fernflower.jar"));
-
             if (StringUtils.lower(entry.getName()).endsWith("fernflower.jar"))
             {
                 ff.getParentFile().mkdirs();
